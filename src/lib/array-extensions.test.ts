df6fd27091d20f94e2dd780204ee1ed782e71a88
Dominic Griesel <d.griesel@gmx.net>
2017-11-15 15:47:50 +0100

    removed tests for removed code

--- src/lib/array-extensions.test.ts
+++ src/lib/array-extensions.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { except, firstOrDefault, intersect, range } from "./array-extensions";
+import { except, firstOrDefault } from "./array-extensions";
 // tslint:disable:no-unused-expression
 
 describe("lib/array-extensions => except() =>", () => {
@@ -44,60 +44,60 @@ describe("lib/array-extensions => except() =>", () => {
 
 });
 
-describe("lib/array-extensions => intersect() =>", () => {
+// describe("lib/array-extensions => intersect() =>", () => {
 
-	it("should work for b[] ⊆ a[]", () => {
-		const a = [1, 3, 5, 7, 8];
-		const b = [1, 5, 8];
-		const expected = b;
-		expect(intersect(a, b)).to.deep.equal(expected);
-	});
+// 	it("should work for b[] ⊆ a[]", () => {
+// 		const a = [1, 3, 5, 7, 8];
+// 		const b = [1, 5, 8];
+// 		const expected = b;
+// 		expect(intersect(a, b)).to.deep.equal(expected);
+// 	});
 
-	it("should work for b[] ⊊ a[]", () => {
-		const a = [1, 3, 5, 7, 8];
-		const b = [1, 5, 8, 9, 11];
-		const expected = [1, 5, 8];
-		expect(intersect(a, b)).to.deep.equal(expected);
-	});
+// 	it("should work for b[] ⊊ a[]", () => {
+// 		const a = [1, 3, 5, 7, 8];
+// 		const b = [1, 5, 8, 9, 11];
+// 		const expected = [1, 5, 8];
+// 		expect(intersect(a, b)).to.deep.equal(expected);
+// 	});
 
-	it("shouldn't depend on the argument order", () => {
-		const a = [1, 3, 5, 7, 8];
-		const b = [1, 5, 8, 9, 11];
-		const expected = [1, 5, 8];
-		expect(intersect(a, b)).to.deep.equal(expected);
-		expect(intersect(a, b)).to.deep.equal(intersect(b, a));
-	});
+// 	it("shouldn't depend on the argument order", () => {
+// 		const a = [1, 3, 5, 7, 8];
+// 		const b = [1, 5, 8, 9, 11];
+// 		const expected = [1, 5, 8];
+// 		expect(intersect(a, b)).to.deep.equal(expected);
+// 		expect(intersect(a, b)).to.deep.equal(intersect(b, a));
+// 	});
 
-	it("should work for empty arrays", () => {
-		const a = [1, 5, 8];
-		const b = [];
-		const c = [4, 5, 6];
-		expect(intersect(a, b)).to.deep.equal([]);
-		expect(intersect(b, c)).to.deep.equal([]);
-		expect(intersect(c, b)).to.deep.equal([]);
-		expect(intersect(b, b)).to.deep.equal([]);
-	});
+// 	it("should work for empty arrays", () => {
+// 		const a = [1, 5, 8];
+// 		const b = [];
+// 		const c = [4, 5, 6];
+// 		expect(intersect(a, b)).to.deep.equal([]);
+// 		expect(intersect(b, c)).to.deep.equal([]);
+// 		expect(intersect(c, b)).to.deep.equal([]);
+// 		expect(intersect(b, b)).to.deep.equal([]);
+// 	});
 
-});
+// });
 
-describe("lib/array-extensions => range() =>", () => {
+// describe("lib/array-extensions => range() =>", () => {
 
-	it("should work", () => {
-		expect(range(1, 7)).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
-	});
-	it("should work for negative values", () => {
-		expect(range(-2, 2)).to.deep.equal([-2, -1, 0, 1, 2]);
-	});
+// 	it("should work", () => {
+// 		expect(range(1, 7)).to.deep.equal([1, 2, 3, 4, 5, 6, 7]);
+// 	});
+// 	it("should work for negative values", () => {
+// 		expect(range(-2, 2)).to.deep.equal([-2, -1, 0, 1, 2]);
+// 	});
 
-	it("should work for reversed min/max", () => {
-		expect(range(4, 1)).to.deep.equal([1, 2, 3, 4]);
-	});
+// 	it("should work for reversed min/max", () => {
+// 		expect(range(4, 1)).to.deep.equal([1, 2, 3, 4]);
+// 	});
 
-	it("should work for min = max", () => {
-		expect(range(1, 1)).to.deep.equal([1]);
-	});
+// 	it("should work for min = max", () => {
+// 		expect(range(1, 1)).to.deep.equal([1]);
+// 	});
 
-});
+// });
 
 describe("lib/array-extensions => firstOrDefault() =>", () => {
 
--- src/lib/object-polyfill.test.ts
+++ src/lib/object-polyfill.test.ts
@@ -1,51 +1,51 @@
 import { expect } from "chai";
-import { bury, composeObject, dig, entries, extend, filter, values } from "./object-polyfill";
+import { composeObject, entries, filter, values } from "./object-polyfill";
 // tslint:disable:no-unused-expression
 
-describe("lib/object-polyfill => bury() =>", () => {
+// describe("lib/object-polyfill => bury() =>", () => {
 
-	it("should work with nested properties", () => {
-		const target = {a: "a", b: {c: "c"}, d: "d"};
-		bury(target, "a", "A");
-		bury(target, "b.c", "C");
-		const expected = {a: "A", b: {c: "C"}, d: "d"};
-		expect(target).to.deep.equal(expected);
-	});
+// 	it("should work with nested properties", () => {
+// 		const target = {a: "a", b: {c: "c"}, d: "d"};
+// 		bury(target, "a", "A");
+// 		bury(target, "b.c", "C");
+// 		const expected = {a: "A", b: {c: "C"}, d: "d"};
+// 		expect(target).to.deep.equal(expected);
+// 	});
 
-	it("should work with non-existing properties", () => {
-		const target = {a: "a", b: {c: "c"}, d: "d"};
-		bury(target, "e", "E");
-		const expected = {a: "a", b: {c: "c"}, d: "d", e: "E"};
-		expect(target).to.deep.equal(expected);
-	});
+// 	it("should work with non-existing properties", () => {
+// 		const target = {a: "a", b: {c: "c"}, d: "d"};
+// 		bury(target, "e", "E");
+// 		const expected = {a: "a", b: {c: "c"}, d: "d", e: "E"};
+// 		expect(target).to.deep.equal(expected);
+// 	});
 
-	it("should work with arrays", () => {
-		const target = {a: [{b: "b"}, {c: "c"}]};
-		bury(target, "a.[1].c", "C");
-		bury(target, "a.[1].d", "d");
-		const expected = {a: [{b: "b"}, {c: "C", d: "d"}]};
-		expect(target).to.deep.equal(expected);
-	});
+// 	it("should work with arrays", () => {
+// 		const target = {a: [{b: "b"}, {c: "c"}]};
+// 		bury(target, "a.[1].c", "C");
+// 		bury(target, "a.[1].d", "d");
+// 		const expected = {a: [{b: "b"}, {c: "C", d: "d"}]};
+// 		expect(target).to.deep.equal(expected);
+// 	});
 
-});
+// });
 
-describe("lib/object-polyfill => dig() =>", () => {
+// describe("lib/object-polyfill => dig() =>", () => {
 
-	const source = {a: "A", b: ["B", {c: "C"}], d: {e: "E"}};
+// 	const source = {a: "A", b: ["B", {c: "C"}], d: {e: "E"}};
 
-	it("should find the correct property", () => {
-		expect(dig(source, "a")).to.equal("A");
-	});
+// 	it("should find the correct property", () => {
+// 		expect(dig(source, "a")).to.equal("A");
+// 	});
 
-	it("should work with nested properties", () => {
-		expect(dig(source, "d.e")).to.equal("E");
-	});
+// 	it("should work with nested properties", () => {
+// 		expect(dig(source, "d.e")).to.equal("E");
+// 	});
 
-	it("should work with arrays", () => {
-		expect(dig(source, "b.[0]")).to.be.equal("B");
-		expect(dig(source, "b.[1]")).to.deep.equal({c: "C"});
-	});
-});
+// 	it("should work with arrays", () => {
+// 		expect(dig(source, "b.[0]")).to.be.equal("B");
+// 		expect(dig(source, "b.[1]")).to.deep.equal({c: "C"});
+// 	});
+// });
 
 describe("lib/object-polyfill => values() =>", () => {
 
@@ -114,37 +114,37 @@ describe("lib/object-polyfill => filter() =>", () => {
 
 });
 
-describe("lib/object-polyfill => extend() =>", () => {
+// describe("lib/object-polyfill => extend() =>", () => {
 
-	it("the target should include the source object's properties", () => {
-		const target1 = {};
-		const target2 = {a: "a"};
-		const target3 = {c: {_c: "c"}};
-		const source = {a: 1, b: 2, c: {d: "d", e: "e"}};
-		expect(extend(target1, source)).to.deep.include(source);
-		expect(extend(target2, source)).to.deep.include(source);
-		expect(extend(target3, source)).to.include({a: 1, b: 2});
-		expect(extend(target3.c, source)).to.include({d: "d", e: "e"});
-	});
+// 	it("the target should include the source object's properties", () => {
+// 		const target1 = {};
+// 		const target2 = {a: "a"};
+// 		const target3 = {c: {_c: "c"}};
+// 		const source = {a: 1, b: 2, c: {d: "d", e: "e"}};
+// 		expect(extend(target1, source)).to.deep.include(source);
+// 		expect(extend(target2, source)).to.deep.include(source);
+// 		expect(extend(target3, source)).to.include({a: 1, b: 2});
+// 		expect(extend(target3.c, source)).to.include({d: "d", e: "e"});
+// 	});
 
-	it("should override existing properties", () => {
-		const target = {a: "a", b: "b"};
-		const source = {a: "A"};
-		const expected = {a: "A", b: "b"};
-		expect(extend(target, source)).to.deep.equal(expected);
-	});
+// 	it("should override existing properties", () => {
+// 		const target = {a: "a", b: "b"};
+// 		const source = {a: "A"};
+// 		const expected = {a: "A", b: "b"};
+// 		expect(extend(target, source)).to.deep.equal(expected);
+// 	});
 
-	it("should merge nested objects", () => {
-		const target = {a: "a", b: {c: "c"}};
-		const source = {b: {d: "d"}};
-		const expected = {a: "a", b: {c: "c", d: "d"}};
-		expect(extend(target, source)).to.deep.equal(expected);
-	});
+// 	it("should merge nested objects", () => {
+// 		const target = {a: "a", b: {c: "c"}};
+// 		const source = {b: {d: "d"}};
+// 		const expected = {a: "a", b: {c: "c", d: "d"}};
+// 		expect(extend(target, source)).to.deep.equal(expected);
+// 	});
 
-	it("should accept null as a target", () => {
-		expect(extend(null, {a: "a"})).to.deep.equal({a: "a"});
-	});
-	it("should work with empty objects", () => {
-		expect(extend({}, {})).to.deep.equal({});
-	});
-});
+// 	it("should accept null as a target", () => {
+// 		expect(extend(null, {a: "a"})).to.deep.equal({a: "a"});
+// 	});
+// 	it("should work with empty objects", () => {
+// 		expect(extend({}, {})).to.deep.equal({});
+// 	});
+// });
