ec7d6b4c995399133edbc9da7a258bdb66fcc7cf
Dominic Griesel <d.griesel@gmx.net>
2018-02-16 19:57:15 +0100

    test brightness conversion

--- src/lib/conversions.test.ts
+++ src/lib/conversions.test.ts
@@ -27,6 +27,41 @@ describe("tradfri/conversions => whiteSpectrum <=> colorX =>", () => {
 
 });
 
+describe("tradfri/conversions => brightness() =>", () => {
+
+	const serialize = serializers.brightness;
+	const deserialize = deserializers.brightness;
+
+	// tslint:disable-next-line:variable-name
+	const testSets_in = [
+		{coap: 0, tradfri: 0},
+		{coap: 1, tradfri: 1},
+		{coap: 68, tradfri: 26.8},
+		{coap: 128, tradfri: 50.4},
+		{coap: 254, tradfri: 100},
+	];
+	// tslint:disable-next-line:variable-name
+	const testSets_out = [
+		{tradfri: 0, coap: 0},
+		{tradfri: 1, coap: 3},
+		{tradfri: 25.89, coap: 66},
+		{tradfri: 55, coap: 140},
+		{tradfri: 100, coap: 254},
+	];
+
+	it(`serialize: 0..100% should map to 0..254`, () => {
+		for (const set of testSets_out) {
+			expect(serialize(set.tradfri)).to.equal(set.coap);
+		}
+	});
+	it(`deserialize: 0..254 should map to 0..100%`, () => {
+		for (const set of testSets_in) {
+			expect(deserialize(set.coap)).to.equal(set.tradfri);
+		}
+	});
+
+});
+
 describe("tradfri/conversions => transitionTime() =>", () => {
 
 	const serialize = serializers.transitionTime;
@@ -48,7 +83,7 @@ describe("tradfri/conversions => transitionTime() =>", () => {
 
 });
 
-describe("tradfri/conversions => rgbToHSV() <=> rgbFromHSV", () => {
+describe("tradfri/conversions => rgbToHSV() <=> rgbFromHSV()", () => {
 
 	it("should correctly convert RGB <=> HSV", () => {
 		const testSets = [
@@ -63,6 +98,25 @@ describe("tradfri/conversions => rgbToHSV() <=> rgbFromHSV", () => {
 			const {h, s, v} = set.hsv;
 			expect(conversions.rgbToHSV(r, g, b)).to.deep.equal(set.hsv, "rgbToHSV did not match the expected values");
 			expect(conversions.rgbFromHSV(h, s, v)).to.deep.equal(set.rgb, "rgbFromHSV did not match the expected values");
+		}
+	});
+
+});
+
+describe("tradfri/conversions => rgbToString() <=> rgbFromString()", () => {
+
+	it("should correctly convert RGB <=> HSV", () => {
+		const testSets = [
+			{rgb: {r: 0, g: 0, b: 0}, hex: "000000"},
+			{rgb: {r: 92, g: 46, b: 23}, hex: "5c2e17"},
+			{rgb: {r: 255, g: 255, b: 255}, hex: "ffffff"},
+			{rgb: {r: 191, g: 0, b: 255}, hex: "bf00ff"},
+			// TODO: Extend this to the predefined colors
+		];
+		for (const set of testSets) {
+			const {r, g, b} = set.rgb;
+			expect(conversions.rgbToString(r, g, b)).to.deep.equal(set.hex, "rgbToString did not match the expected values");
+			expect(conversions.rgbFromString(set.hex)).to.deep.equal(set.rgb, "rgbFromString did not match the expected values");
 		}
 	});
 
