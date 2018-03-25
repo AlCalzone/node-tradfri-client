53db618e29a7a049c908a9212c930313b23f0611
Al Calzone <d.griesel@gmx.net>
2018-03-25 17:20:12 +0200

    next attempt without restoring the objects to the faulty state this might be crashing the android app

--- src/tradfri-client.ts
+++ src/tradfri-client.ts
@@ -823,9 +823,9 @@ export class TradfriClient extends EventEmitter implements OperationProvider {
 		// ensure the ipso options were not lost on the user side
 		newObj.options = this.ipsoOptions;
 
-		// create clones with bugged properties reset to how the gateway expects them
-		newObj = newObj.clone().restoreBuggedProperties();
-		reference = reference.clone().restoreBuggedProperties();
+		// // create clones with bugged properties reset to how the gateway expects them
+		// newObj = newObj.clone().restoreBuggedProperties();
+		// reference = reference.clone().restoreBuggedProperties();
 
 		log(`updateResource(${path}) > comparing ${JSON.stringify(newObj)} with the reference ${JSON.stringify(reference)}`, "debug");
 
