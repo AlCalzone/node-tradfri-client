841e5601995bee529d52c66a6b208a86aa39dd4d
dominic.griesel <d.griesel@gmx.net>
2018-08-24 15:48:49 +0200

    Add a todo list for fixing discovery

--- src/lib/discovery.ts
+++ src/lib/discovery.ts
@@ -1,3 +1,9 @@
+// This is buggy...
+// TODO:
+// - [ ] use multicast-dns package
+// - [ ] manually cycle interfaces and set opts.interface (IPv6 with %<name> suffix)
+// - [ ] use the following opts.ip: IPv4: 224.0.0.251, IPv6: FF02::FB
+
 import * as createMDNSServer from "mdns-server";
 
 export interface DiscoveredGateway {
