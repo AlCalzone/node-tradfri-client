1a3baa18714f828bc87350895b035c974dc76ca0
dominic.griesel <d.griesel@gmx.net>
2018-09-20 14:42:38 +0200

    Fix tests under the new strictness flags

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -1,5 +1,7 @@
 // Makes ts-node ignore warnings, so mocha --watch does work
 process.env.TS_NODE_IGNORE_WARNINGS = "TRUE";
+// Sets the correct tsconfig for testing
+process.env.TS_NODE_PROJECT = "tsconfig.testing.json";
 
 // Don't silently swallow unhandled rejections
 process.on("unhandledRejection", (e) => {
